cbeb496ff9c6c1aa25c7a79828efab9d29e79af3
linziyi <linziyi_nanjin@sina.com>
2026-08-27 21:05:40 +0800

    feat:new

--- hello_node.js
+++ hello_node.js
@@ -2,12 +2,12 @@
  * @Author: linziyi linziyi_nanjin@sina.com
  * @Date: 2026-08-27 10:20:35
  * @LastEditors: linziyi linziyi_nanjin@sina.com
- * @LastEditTime: 2026-08-27 15:51:46
+ * @LastEditTime: 2026-08-27 17:01:13
  * @FilePath: \node.js\hello_node.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import express from "express";
-import fs from "fs";
+import fs from "node:fs/promises";
 
 const post = 3000;
 const app = express();
@@ -15,29 +15,26 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 console.log("hello node");
-
+//async函数会返回Promise，函数内部:  return sever相当于resove(server),  throw error相当于reject(error) 
 async function server() {
-    try {
-        const data = await fs.readFileSync("./input.txt", "utf8");
-        const tell = data.toString();;
 
-        const server = await app.post("/", function (request, response) {
-            const message = request.body.message.trim();
-            if (!tell) {
-                throw new Error("tell空了");
-            }
-            response.status(200).json({ "answer": `你好${message}     ${tell}` });
-        });
-        return new Promise((resolve, reject) => {
-            resolve(server);
-        });
-    } catch (err) {
-        return new Promise((resolve, reject) => {
-            reject("出错了:" + err);
-        });
-    }
+    const [data_1, data_2] = await Promise.all([//readFile中定义了编码方式为utf8,所所以readFile返回的是字符串
+        fs.readFile("./input_1.txt","utf8"),
+        fs.readFile("./input_2.txt","utf8")//同时开始，全部结束后才会进行下一步
+    ])
 
-};
+    const server = app.post("/", function (request, response) {
+        const message = request.body.message.trim();
+        if (!data_1 || !data_2) {
+            throw new Error("data空了");
+        }
+        response.status(200).json({
+            "answer": `你好${message}======>${data_1}`,
+            "add": `${data_2}`
+        });
+    });
+    return server;
+}
 
 const result = server();
 result.then(
@@ -47,6 +44,6 @@ result.then(
             // console.log(resolve);
         })
     }
-).catch((reject)=>{
-    console.log(reject);
+).catch((reject) => {
+    reject instanceof Error ? console.log("出错了：" + reject.message) : console.log("未知错误");
 })
